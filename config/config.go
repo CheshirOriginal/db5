ab194b811a48d57effac55dcb227e95a91379b09
Kirya <hghghghgh@mail.ru>
2025-05-03 18:08:00 +0300

    added basic project structure

---
+++ config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBUser string
+	DBPass string
+	DBHost string
+	DBPort string
+	DBName string
+}
+
+func LoadConfig() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env не найден, продолжаем без него")
+	}
+
+	cfg := Config{
+		DBUser: os.Getenv("DB_USER"),
+		DBPass: os.Getenv("DB_PASS"),
+		DBHost: os.Getenv("DB_HOST"),
+		DBPort: os.Getenv("DB_PORT"),
+		DBName: os.Getenv("DB_NAME"),
+	}
+
+	// Простейшая проверка
+	if cfg.DBUser == "" || cfg.DBPass == "" {
+		log.Fatal("DB_USER или DB_PASS не заданы в .env")
+	}
+
+	return cfg
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module db5
 
 go 1.24
+
+require github.com/joho/godotenv v1.5.1
